c0cbb82e62549e950d5b13dda5b1daf515c9e9ab
Ville4 <krutilinville@gmail.com>
2023-04-02 00:04:11 +0300

    fix related to correct page refresh on gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,16 @@ import store from './redux/redux-store';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 
